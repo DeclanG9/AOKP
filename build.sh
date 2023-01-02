21f50860011bf6de249ee49637506e7807b705b6
DeclanG9 <kanozx23@gmail.com>
2023-01-02 21:45:52 +0700

    user

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ export KBUILD_BUILD_USER=$KBUILD_BUILD_USER
 export KBUILD_BUILD_HOST=$KBUILD_BUILD_USER
 export BUILD_USERNAME=$KBUILD_BUILD_USER
 export BUILD_HOSTNAME=$KBUILD_BUILD_USER
-lunch derp_beryllium-userdebug
+lunch derp_beryllium-user
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
