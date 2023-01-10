ec7d5d03b31ee0a626749e96d3c1fcec11dbe49e
DeclanG9 <kanozx23@gmail.com>
2023-01-10 16:40:30 +0700

    aokp

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone --depth 1 https://github.com/DeclanG9/local_manifest -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
